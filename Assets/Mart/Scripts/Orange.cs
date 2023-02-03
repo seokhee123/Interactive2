3155d7d515ea2532227e7c0b9cb02203e169a475
seokhee <seokhee1203@naver.com>
2023-02-03 10:58:25 +0900

    순서변경

--- Assets/Mart/Scripts/Orange.cs
+++ Assets/Mart/Scripts/Orange.cs
@@ -6,9 +6,11 @@ public class Orange : MonoBehaviour
 {
     public GameObject earth;
     public GameObject cloud;
+    public GameObject orange;
     private void OnMouseDown()
     {
         earth.SetActive(true);
         cloud.SetActive(true);
+        orange.SetActive(true);
     }
 }
--- Assets/Mart/Scripts/OrangeBox.cs
+++ Assets/Mart/Scripts/OrangeBox.cs
@@ -6,10 +6,14 @@ using UnityEngine.UIElements;
 public class OrangeBox : MonoBehaviour
 {
     public GameObject box;
-
+    Animator anim;
+    private void Awake()
+    {
+        anim = box.GetComponent<Animator>();
+    }
     private void OnMouseDown()
     {
-        box.SetActive(true);
+        anim.enabled = true;
 
     }
 }
