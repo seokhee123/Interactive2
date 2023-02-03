ce0c25872e74b53a9cb900c85497f9331083109c
seokhee <seokhee1203@naver.com>
2023-02-03 15:11:09 +0900

    애니메이션 timescale
    애니메이션 timescale 추가

--- Assets/Mart/Scripts/Orange.cs
+++ Assets/Mart/Scripts/Orange.cs
@@ -7,10 +7,19 @@ public class Orange : MonoBehaviour
     public GameObject earth;
     public GameObject cloud;
     public GameObject orange;
+    bool isNext = false;
     private void OnMouseDown()
     {
-        earth.GetComponent<Animator>().enabled = true;
+        if (isNext == true)
+        {
+            earth.GetComponent<Animator>().enabled = true;
+        }
+        gameObject.GetComponent<Animator>().enabled = true;
+        earth.SetActive(true);
+        
         cloud.SetActive(true);
         orange.SetActive(true);
+        isNext = true;
+
     }
 }
