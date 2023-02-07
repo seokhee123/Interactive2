63a98f4d7032005629098edae2063cb85f17bc34
seokhee <seokhee1203@naver.com>
2023-02-07 13:24:01 +0900

    주방 Scene 추가 + particle 추가

---
+++ Assets/kitchen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kitchen : MonoBehaviour
+{
+    public GameObject junkFood;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+
+    void TimeScale()
+    {
+        Time.timeScale = 0;
+    }
+}
