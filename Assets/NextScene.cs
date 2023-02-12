63080c880aeacbdb5998f8ee403163a15c9c6d49
seokhee123 <82377493+seokhee123@users.noreply.github.com>
2023-02-13 00:44:52 +0900

    잠자리, 벌 움직임 추가

--- Assets/NextScene.cs
+++ Assets/NextScene.cs
@@ -14,9 +14,11 @@ public class NextScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            SceneManager.LoadScene("Kitchen");
-        }
+        
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Kitchen");
     }
 }
--- Assets/beePath.cs
+++ Assets/beePath.cs
@@ -5,7 +5,7 @@ using PathCreation;
 
 public class beePath : MonoBehaviour
 {
-    public PathCreator pathCreator;
+    /*public PathCreator pathCreator;
     public float speed = 1f;
     float distanceTravel;
 
@@ -14,5 +14,21 @@ public class beePath : MonoBehaviour
         distanceTravel += speed * Time.deltaTime;
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravel);
         transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravel);
+    }
+    */
+
+    public Transform Target;
+    public bool isbee;
+
+    void Update()
+    {
+        if (isbee)
+        {
+            transform.RotateAround(Target.position, Vector3.forward, 20 * Time.deltaTime);
+        }
+        else
+        {
+            transform.RotateAround(Target.position, Vector3.back, 20 * Time.deltaTime);
+        }
     }
 }
