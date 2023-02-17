e777587f7e505c9639fb05b00169401aa80f51fe
seokhee <seokhee1203@naver.com>
2023-02-17 16:57:00 +0900

    푸드마일리지 애니메이션

--- Assets/FoodBeef.cs
+++ Assets/FoodBeef.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class FoodBeef : MonoBehaviour
 {
     public GameObject beefTruck;
+    public AudioSource sounds;
 
     private void OnMouseDown()
     {
         beefTruck.GetComponent<Animator>().enabled = true;
+        sounds.Play();
     }
+
 }
